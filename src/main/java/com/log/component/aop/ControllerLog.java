62922c384b1bc8e314e9f890e27828cea86ab0e2
Your Name <you@example.com>
2022-11-04 15:44:48 +0800

    整理日志输出格式

--- src/main/java/com/log/component/aop/ControllerLog.java
+++ src/main/java/com/log/component/aop/ControllerLog.java
@@ -1,11 +1,11 @@
 package com.log.component.aop;
 
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.Signature;
 import org.aspectj.lang.annotation.*;
 import org.aspectj.lang.reflect.MethodSignature;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
@@ -15,7 +15,7 @@ import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Date;
 
-@Aspect  // 使用@Aspect注解声明一个切面
+@Aspect
 @Component("controllerLog")
 @Slf4j
 public class ControllerLog {
@@ -27,18 +27,18 @@ public class ControllerLog {
 
     @Around(value = "logPointCut()")
     public Object around(ProceedingJoinPoint point) throws Throwable {
-        Object result = before(point);
-        return result;
+        return process(point);
     }
 
-    public Object before(ProceedingJoinPoint point) throws Throwable {
-        log.info("==========================  " + getCurrentMethodName(point) + " start =============================");
+    public Object process(ProceedingJoinPoint point) throws Throwable {
+        System.out.println("\n");
+        log.info("==========================  " + getCurrentMethodName(point) + " starting =============================");
         long beginTime = System.currentTimeMillis();
         long time = System.currentTimeMillis() - beginTime;
         ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
         HttpServletRequest request = null;
         if (attributes.getRequest() != null) {
-            request = (HttpServletRequest) attributes.getRequest();
+            request = attributes.getRequest();
         }
         log.info("API ：" + request.getRequestURI());
         log.info("类方法 : " + point.getSignature().getDeclaringTypeName() + "." + point.getSignature().getName());
@@ -50,15 +50,29 @@ public class ControllerLog {
         log.info("开始时间：" + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss:sss").format(new Date()));
         log.info("结束时间：" + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss:sss").format(new Date()));
         log.info("耗时 : " + time);
-        after(point);
+        log.info("==========================  " + getCurrentMethodName(point) + " ending =============================\n");
         return result;
     }
 
-    private void after(ProceedingJoinPoint point) {
+//    @Before(value = "logPointCut()")
+//    private void before(JoinPoint point) {
+//        log.info("==========================  " + getCurrentMethodName(point) + " starting =============================");
+//    }
+
+//    @After(value = "logPointCut()")
+//    private void after(JoinPoint point) {
+//        log.info("==========================  " + getCurrentMethodName(point) + " ending =============================");
+//
+//    }
+
+    @AfterThrowing(value = "logPointCut()", throwing = "e")
+    private void throwAfter(JoinPoint point, Exception e) {
+        log.error(e.getMessage());
         log.info("==========================  " + getCurrentMethodName(point) + " ending =============================\n");
+
     }
 
-    public String getCurrentMethodName(ProceedingJoinPoint point) {
+    public String getCurrentMethodName(JoinPoint point) {
         Signature signature = point.getSignature();
         MethodSignature methodSignature = (MethodSignature) signature;
         Object target = point.getTarget();
@@ -69,7 +83,7 @@ public class ControllerLog {
         }
     }
 
-    public Object[] getParams(ProceedingJoinPoint point) {
+    public Object[] getParams(JoinPoint point) {
         if (point.getArgs().length == 0) {
             return new Object[]{};
         }
